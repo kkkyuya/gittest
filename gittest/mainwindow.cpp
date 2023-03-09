9a13f157298eed8f01111b1f6d30ef8354574f30
kkkyuya <katoyuya1008@gmail.com>
2023-03-09 14:13:01 +0900

    ボタン編集

--- gittest/mainwindow.cpp
+++ gittest/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,19 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    qDebug() << "melon　clicked()!";
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    qDebug() << "　apple clicked()!";
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    qDebug() << "strawbery　clicked()!";
 }
